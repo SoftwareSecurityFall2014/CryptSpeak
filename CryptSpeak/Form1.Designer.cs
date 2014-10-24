f16a882f1ea22ff8b7a9fcabee947d66a61caf66
William Holder <holderwd93@hotmail.com>
2014-10-24 15:28:39 -0500

    Removed the local file location

--- CryptSpeak/Form1.Designer.cs
+++ CryptSpeak/Form1.Designer.cs
@@ -48,77 +48,86 @@
             // 
             // tbYourIP
             // 
-            this.tbYourIP.Location = new System.Drawing.Point(80, 6);
+            this.tbYourIP.Location = new System.Drawing.Point(60, 5);
+            this.tbYourIP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbYourIP.Name = "tbYourIP";
-            this.tbYourIP.Size = new System.Drawing.Size(139, 22);
+            this.tbYourIP.Size = new System.Drawing.Size(105, 20);
             this.tbYourIP.TabIndex = 0;
             this.tbYourIP.Text = "127.0.0.1";
             // 
             // tbEndIP
             // 
-            this.tbEndIP.Location = new System.Drawing.Point(80, 34);
+            this.tbEndIP.Location = new System.Drawing.Point(60, 28);
+            this.tbEndIP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbEndIP.Name = "tbEndIP";
-            this.tbEndIP.Size = new System.Drawing.Size(139, 22);
+            this.tbEndIP.Size = new System.Drawing.Size(105, 20);
             this.tbEndIP.TabIndex = 2;
             this.tbEndIP.Text = "127.0.0.1";
             // 
             // tbYourPort
             // 
-            this.tbYourPort.Location = new System.Drawing.Point(273, 6);
+            this.tbYourPort.Location = new System.Drawing.Point(205, 5);
+            this.tbYourPort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbYourPort.Name = "tbYourPort";
-            this.tbYourPort.Size = new System.Drawing.Size(60, 22);
+            this.tbYourPort.Size = new System.Drawing.Size(46, 20);
             this.tbYourPort.TabIndex = 1;
             this.tbYourPort.Text = "80";
             // 
             // tbEndPort
             // 
-            this.tbEndPort.Location = new System.Drawing.Point(273, 34);
+            this.tbEndPort.Location = new System.Drawing.Point(205, 28);
+            this.tbEndPort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbEndPort.Name = "tbEndPort";
-            this.tbEndPort.Size = new System.Drawing.Size(60, 22);
+            this.tbEndPort.Size = new System.Drawing.Size(46, 20);
             this.tbEndPort.TabIndex = 3;
             this.tbEndPort.Text = "80";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.Size = new System.Drawing.Size(48, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Your IP: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Location = new System.Drawing.Point(9, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "End IP: ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(225, 9);
+            this.label3.Location = new System.Drawing.Point(169, 7);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Port: ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(225, 37);
+            this.label4.Location = new System.Drawing.Point(169, 30);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.Size = new System.Drawing.Size(32, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "Port: ";
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(259, 69);
+            this.btnConnect.Location = new System.Drawing.Point(194, 56);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.Size = new System.Drawing.Size(56, 19);
             this.btnConnect.TabIndex = 9;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -126,16 +135,18 @@
             // 
             // tbToSend
             // 
-            this.tbToSend.Location = new System.Drawing.Point(16, 300);
+            this.tbToSend.Location = new System.Drawing.Point(12, 244);
+            this.tbToSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbToSend.Name = "tbToSend";
-            this.tbToSend.Size = new System.Drawing.Size(237, 22);
+            this.tbToSend.Size = new System.Drawing.Size(179, 20);
             this.tbToSend.TabIndex = 10;
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(258, 300);
+            this.btnSend.Location = new System.Drawing.Point(194, 244);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.Size = new System.Drawing.Size(56, 19);
             this.btnSend.TabIndex = 11;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -144,54 +155,55 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(339, 34);
+            this.listBox1.Location = new System.Drawing.Point(254, 28);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(230, 260);
+            this.listBox1.Size = new System.Drawing.Size(174, 212);
             this.listBox1.TabIndex = 12;
             // 
             // tbKeyFile
             // 
-            this.tbKeyFile.Location = new System.Drawing.Point(385, 303);
+            this.tbKeyFile.Location = new System.Drawing.Point(289, 246);
+            this.tbKeyFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbKeyFile.Name = "tbKeyFile";
-            this.tbKeyFile.Size = new System.Drawing.Size(184, 22);
+            this.tbKeyFile.Size = new System.Drawing.Size(139, 20);
             this.tbKeyFile.TabIndex = 13;
-            this.tbKeyFile.Text = "C:/Users/William/Documents/Visual Studio 2012/Projects/CryptSpeak/CryptSpeak/Key2" +
-    ".txt";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(339, 303);
+            this.label5.Location = new System.Drawing.Point(254, 246);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 17);
+            this.label5.Size = new System.Drawing.Size(31, 13);
             this.label5.TabIndex = 14;
             this.label5.Text = "Key: ";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(339, 9);
+            this.label6.Location = new System.Drawing.Point(254, 7);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(82, 17);
+            this.label6.Size = new System.Drawing.Size(62, 13);
             this.label6.TabIndex = 15;
             this.label6.Text = "Encryptions";
             // 
             // lbMessages
             // 
             this.lbMessages.FormattingEnabled = true;
-            this.lbMessages.ItemHeight = 16;
-            this.lbMessages.Location = new System.Drawing.Point(16, 98);
+            this.lbMessages.Location = new System.Drawing.Point(12, 80);
+            this.lbMessages.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lbMessages.Name = "lbMessages";
-            this.lbMessages.Size = new System.Drawing.Size(318, 196);
+            this.lbMessages.Size = new System.Drawing.Size(240, 160);
             this.lbMessages.TabIndex = 16;
             // 
-            // Form1
+            // UIMessenger
             // 
             this.AcceptButton = this.btnSend;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 333);
+            this.ClientSize = new System.Drawing.Size(436, 271);
             this.Controls.Add(this.lbMessages);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -208,7 +220,8 @@
             this.Controls.Add(this.tbYourPort);
             this.Controls.Add(this.tbEndIP);
             this.Controls.Add(this.tbYourIP);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "UIMessenger";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
